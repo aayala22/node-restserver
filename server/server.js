638f54e771c4110be94d3f6e4b08143a807e22c8
Alexander <aayala22@misena.edu.co>
2018-09-05 08:57:21 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+require('./config/config.js');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app
+    .get('/usuarios', function(req, res) {
+        res.json('get usuarios')
+    })
+    .post('/usuarios', function(req, res) {
+
+        let body = req.body;
+
+        if (body.nombre == undefined) {
+            res.status(400).json({
+                ok: false,
+                mensaje: `El nombre es necesario`
+            });
+        } else {
+            res.json({
+                persona: body
+            })
+        }
+    })
+    .put('/usuarios/:id', function(req, res) {
+        let identificacion = req.params.id;
+        res.json({
+            identificacion: identificacion
+        })
+    })
+    .delete('/usuarios', function(req, res) {
+        res.json('delete usuarios')
+    })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Ejecutando en el puerto ${process.env.PORT}`);
+})
